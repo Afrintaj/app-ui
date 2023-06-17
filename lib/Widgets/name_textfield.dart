20a68f25e4ffd3dcee29d2fed4792866299b955d
Afrintaj <afrintajr786@gmail.com>
2023-06-17 18:23:23 +0530

    validation added

--- lib/Widgets/name_textfield.dart
+++ lib/Widgets/name_textfield.dart
@@ -1,9 +1,21 @@
 import 'package:flutter/material.dart';
 
 Widget nameTextField(
-    {required TextEditingController controller, required Function onChange}) {
-  return TextField(
+    {required TextEditingController controller,
+    required Function onChange,
+    required Function validate}) {
+  return TextFormField(
     controller: controller,
+    decoration: const InputDecoration(
+      border: OutlineInputBorder(),
+    ),
+    validator: (value) {
+      if (value!.isEmpty) {
+        return "Field is Empty";
+      }
+      validate(value);
+      return null;
+    },
     onChanged: (value) {
       onChange(value);
     },
